2678979cc98cca220fd2b0eac319755af45f3717
Skylar Gifford <sgiffio@me.com>
2014-12-21 15:52:24 -0500

    Comments & Update Var
    wrote commends for cacheSolve function & updated var for getinv

--- cachematrix.R
+++ cachematrix.R
@@ -24,10 +24,13 @@ makeCacheMatrix <- function(x = matrix()){
      setinv <- function(inverse) i <<- inverse
      getinv <- function() i 
      list(set = set, get = get, 
-          setinv = setinv, getinv = get inv)
+          setinv = setinv, getinv = getinv)
 }
 
 ## Write a short comment describing this function
+## Computes the inverse of the special matrix returned by the above func
+## If the inv is already calculated (and matrix is the same) then will 
+## retrieve inv from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
